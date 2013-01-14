0fb2cf38e645e131efb0cf89266bd3308e45d0e2
Carlos Augusto <guto.pro@gmail.com>
2013-01-14 02:12:01 -0200

    Primeiro commit

---
+++ Aula0/Aula0Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Aula0Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HelloWorld());
+        }
+        public static string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ Aula0/Aula0Test/Aula0Test.cs
@@ -0,0 +1,14 @@
+﻿using Aula0Console;
+using NUnit.Framework;
+
+namespace Aula0Test
+{
+    public class Aula0Test
+    {
+        [Test]
+        public void HelloWorldTest()
+        {
+            Assert.AreEqual(Program.HelloWorld(),"Hello World");
+        }
+    }
+}
